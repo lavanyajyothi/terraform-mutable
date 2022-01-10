48b946756031ab3d0abb6f532cd08d95a86ac6b3
lavanya <lavanya.bhadri>
2022-01-10 22:43:45 +0530

    adding private and public subnets

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private_subnets" {
+resource "aws_subnet" "private-subnets" {
   depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
   count      = length(var.PRIVATE_SUBNETS)
   vpc_id     = aws_vpc.main.id
@@ -9,7 +9,7 @@ resource "aws_subnet" "private_subnets" {
     Name = "private-subnet-${count.index}"
   }
 }
-resource "aws_subnet" "public_subnets" {
+resource "aws_subnet" "public-subnets" {
   depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
   count      = length(var.PUBLIC_SUBNETS)
   vpc_id     = aws_vpc.main.id
