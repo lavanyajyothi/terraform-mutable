0b57ecb1bf2ee8835fac0168d27bbc89311ee020
lavanya <lavanya.bhadri>
2022-01-10 07:20:22 +0530

    route tables

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -10,8 +10,8 @@ resource "aws_subnet" "subnets" {
   }
 }
 
-resource "aws_route_table_association" "priv-assoc" {
-  count          = length(aws_subnet.private-subnets.*.id)
-  subnet_id      = element(aws_subnet.private-subnets.*.id, count.index)
-  route_table_id = aws_route_table.private-route.id
+resource "aws_route_table_association" "assoc" {
+  count          = length(aws_subnet.subnets.*.id)
+  subnet_id      = element(aws_subnet.subnets.*.id, count.index)
+  route_table_id = aws_route_table.route.id
 }
