be502101fa04814cf820e73439c0f761204c7318
lavanya <lavanya.bhadri>
2022-01-12 08:27:48 +0530

    output

--- db/data.tf
+++ db/data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-b60"
-    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
+    bucket = "terraform-badri60"
+    key    = "terraform-mutable/db/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
