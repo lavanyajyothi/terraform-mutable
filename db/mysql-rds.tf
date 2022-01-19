665203a5a4d48adc10079c67276f25a8f24f3586
lavanya <lavanya.bhadri>
2022-01-19 10:08:57 +0530

    output

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,16 +1,13 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
-  identifier             = "mysql-${var.ENV}"
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
   name                   = "dummy"
-  username               = local.rds_user
-  password               = local.rds_pass
+  username               = "admin"
+  password               = "admin123"
   parameter_group_name   = aws_db_parameter_group.pg.name
   skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.mysql.id]
-  db_subnet_group_name   = aws_db_subnet_group.subnet-group.name
  }
 
 resource "aws_db_parameter_group" "pg" {
