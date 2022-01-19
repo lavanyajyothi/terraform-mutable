d8be86a1470423a581c7d7e7f995d868a25ef308
lavanya <lavanya.bhadri>
2022-01-19 10:06:27 +0530

    output

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,22 +1,23 @@
-## resource "aws_db_instance" "mysql" {
-##  allocated_storage      = 10
-##  identifier             = "mysql-${var.ENV}"
-##  engine                 = "mysql"
-##  engine_version         = "5.7"
-##  instance_class         = "db.t3.micro"
-##  name                   = "dummy"
-##  username               = local.rds_user
-###  password               = local.rds_pass
-##  parameter_group_name   = aws_db_parameter_group.pg.name
-##  skip_final_snapshot    = true
-##  vpc_security_group_ids = [aws_security_group.mysql.id]
-##  db_subnet_group_name   = aws_db_subnet_group.subnet-group.name
-## }
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 10
+  identifier             = "mysql-${var.ENV}"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  name                   = "dummy"
+  username               = local.rds_user
+  password               = local.rds_pass
+  parameter_group_name   = aws_db_parameter_group.pg.name
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.mysql.id]
+  db_subnet_group_name   = aws_db_subnet_group.subnet-group.name
+ }
 
 resource "aws_db_parameter_group" "pg" {
   name   = "mysql-${var.ENV}-pg"
   family = "mysql5.7"
 }
+
 resource "aws_db_subnet_group" "subnet-group" {
   name       = "mysqldb-subnet-group-${var.ENV}"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS
