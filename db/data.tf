6774e459f87c044ee2d144ea2ad402d7f5e3f46e
lavanya <lavanya.bhadri>
2022-01-19 11:55:39 +0530

    output

--- db/data.tf
+++ db/data.tf
@@ -11,7 +11,12 @@ data "aws_secretsmanager_secret" "secrets" {
   name  = var.ENV
 }
 
-output "sec" {
-  value = data.aws_secretsmanager_secret.secrets
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+resource  "local_file" "foo" {
+  content  = jsoncode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  filename = "/tmp/1"
 }
 
