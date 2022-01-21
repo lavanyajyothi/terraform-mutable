2fe77a148a38d1c60120acbca99fe1abb9dde912
lavanya <lavanya.bhadri>
2022-01-21 16:37:27 +0530

    output

--- db/mongo-documentdb.tf
+++ db/mongo-documentdb.tf
@@ -1,7 +1,6 @@
 locals {
-  mongo_user = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_USER"]
-
-  mongo_pass = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_PASS"]
+  mongo_user              = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_USER"]
+  mongo_pass              = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_PASS"]
 }
 
 
