e480cb325c2da4fb7fe4b68e7d14904e3457c2f2
lavanya <lavanya.bhadri>
2022-01-21 16:47:16 +0530

    output

--- db/locals.tf
+++ db/locals.tf
@@ -4,3 +4,4 @@ locals {
   ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
   ssh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
 }
+
