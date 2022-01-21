015e6db7825cce2d34638dfee0bcba5d55e0b6fa
lavanya <lavanya.bhadri>
2022-01-21 17:05:48 +0530

    output

--- db/mongo-documentdb.tf
+++ db/mongo-documentdb.tf
@@ -1,14 +1,14 @@
 locals {
-  ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
-  ssh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
+  mongo_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_USER"]
+  mongo_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["MONGO_PASS"]
 }
 
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "mongodb-${var.ENV}"
   engine                  = "docdb"
   engine_version          = "4.0.0"
-  master_username         = local.ssh_user
-  master_password         = local.ssh_pass
+  master_username         = local.mongo_user
+  master_password         = local.mongo_pass
   skip_final_snapshot     = true
 }
 
