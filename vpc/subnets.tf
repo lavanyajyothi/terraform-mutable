91ba8fab4362e248ea445b2fb8e0d21ec8a3c168
lavanya <lavanya.bhadri>
2022-01-25 15:05:40 +0530

    route

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "private-subnets" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count      = length(var.PRIVATE_SUBNETS)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PRIVATE_SUBNETS,count.index)
+  depends_on         = [aws_vpc_ipv4_cidr_block_association.addon]
+  count              = length(var.PRIVATE_SUBNETS)
+  vpc_id             = aws_vpc.main.id
+  cidr_block         = element(var.PRIVATE_SUBNETS,count.index)
   availability_zone  = element(var.AZS, count.index)
 
   tags = {
@@ -10,16 +10,17 @@ resource "aws_subnet" "private-subnets" {
   }
 }
 resource "aws_subnet" "public-subnets" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count      = length(var.PUBLIC_SUBNETS)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNETS,count.index)
+  depends_on         = [aws_vpc_ipv4_cidr_block_association.addon]
+  count              = length(var.PUBLIC_SUBNETS)
+  vpc_id             = aws_vpc.main.id
+  cidr_block         = element(var.PUBLIC_SUBNETS,count.index)
   availability_zone  = element(var.AZS, count.index)
 
   tags = {
     Name = "public-subnet-${count.index}"
   }
 }
+
 
 
 resource "aws_route_table_association" "priv-assoc" {
