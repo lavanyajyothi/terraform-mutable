13d5a8c81e423be6c48d1b9c7efaeec151164faa
lavanya <lavanya.bhadri>
2022-01-26 08:22:32 +0530

    alb

--- alb/public-alb.tf
+++ alb/public-alb.tf
@@ -19,24 +19,3 @@ resource "aws_route53_record" "frontend-public" {
   ttl     = "300"
   records = [aws_lb.public.dns_name]
 }
-resource "aws_lb" "public" {
-  name               = "roboshop-public-${var.ENV}"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.public-alb.id]
-  subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS
-
-  enable_deletion_protection = false
-
-  tags = {
-    Environment = "roboshop-public-${var.ENV}"
-  }
-}
-
-resource "aws_route53_record" "frontend-public" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTEDZONE_ID
-  name    = "roboshop-${var.ENV}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.public.dns_name]
-}
