ac9fb44c22fd1d64b09ac51dbe53f9e284a13436
lavanya <lavanya.bhadri>
2022-01-28 19:26:16 +0530

    instance tags

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -1,9 +1,13 @@
 resource "aws_spot_instance_request" "spot" {
-  count                = var.SPOT_INSTANCE_COUNT
-  ami                  = data.aws_ami.ami.id
-  instance_type        = var.INSTANCE_TYPE
-  subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
-  wait_for_fulfillment = true
+  count                  = var.SPOT_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.INSTANCE_TYPE
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  wait_for_fulfillment   = true
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = local.tags["Name"]
+  }
 }
 
 resource "aws_instance" "od" {
@@ -11,15 +15,9 @@ resource "aws_instance" "od" {
   ami                  = data.aws_ami.ami.id
   instance_type        = var.INSTANCE_TYPE
   subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
-
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
-locals {
-  INSTANCE_IDS     = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
-  tags = {
-    Name = "${var.COMPONENT}-${var.ENV}"
-  }
-}
 
 output "INSTANCE_IDS" {
   value = local.INSTANCE_IDS
