d866cc119ac039f195a53165542b12d234bc3af0
lavanya <lavanya.bhadri>
2022-01-28 19:30:56 +0530

    instance tags

--- app-module/locals.tf
+++ app-module/locals.tf
@@ -2,11 +2,12 @@ locals {
   DEFAULT_VPC_CIDR = split(",", data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
   ALL_VPC_CIDR     = data.terraform_remote_state.vpc.outputs.ALL_VPC_CIDR
   ALL_CIDR         = concat(data.terraform_remote_state.vpc.outputs.ALL_VPC_CIDR, local.DEFAULT_VPC_CIDR)
-  ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
-  ssh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
+ ## ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
+ ## ssh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
   INSTANCE_IDS     = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
   }
 }
+
 
