c04ad40afed8593ae6d8c96c03fee21728b5a864
lavanya <lavanya.bhadri>
2022-01-28 18:28:15 +0530

    instance ids

--- app-module/main.tf
+++ app-module/main.tf
@@ -6,3 +6,20 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment = true
 }
 
+resource "aws_instance" "od" {
+  count                = var.OD_INSTANCE_COUNT
+  ami                  = data.aws_ami.ami.id
+  instance_type        = var.INSTANCE_TYPE
+  subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+
+}
+
+locals {
+  INSTANCE_IDS     = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
+}
+
+output "INSTANCE_IDS" {
+  value = local.INSTANCE_IDS
+}
+
+}
