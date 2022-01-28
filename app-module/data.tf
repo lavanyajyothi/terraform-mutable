60a35cee451ecd2eb85efdcb2329718bb106bcfc
lavanya <lavanya.bhadri>
2022-01-28 18:09:27 +0530

    variables

--- app-module/data.tf
+++ app-module/data.tf
@@ -1,11 +1,11 @@
-##data "terraform_remote_state" "vpc" {
-##  backend = "s3"
-##  config = {
-##    bucket = "terraform-badri60"
-##    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
-##    region = "us-east-1"
-##  }
-##}
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-badri60"
+    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 data "aws_ami" "ami" {
   most_recent = true
