479e7ed448512c3fbae3689765dbbe0263901004
lavanya <lavanya.bhadri>
2022-01-28 19:57:35 +0530

    deploy

--- app-module/locals.tf
+++ app-module/locals.tf
@@ -5,7 +5,7 @@ locals {
   ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
   ssh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
   INSTANCE_IDS     = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
-  PRIVATE_IPS      = concat(aws_spot_instance_request.spot.*private_ip, aws_instance.od.*.private_ip)
+  PRIVATE_IPS      = concat(aws_spot_instance_request.spot.*.private_ip, aws_instance.od.*.private_ip)
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
   }
