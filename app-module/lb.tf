80cac8ad20110f8dc467a43e7d9712a3e5fdaf59
lavanya <lavanya.bhadri>
2022-01-29 08:08:50 +0530

    LISTENER

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -3,6 +3,15 @@ resource "aws_lb_target_group" "tg" {
   port     = var.PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    unhealthy_threshold = 2
+    timeout             = 4
+  }
+}
 
 }
 
