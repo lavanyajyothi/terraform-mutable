19789674175166f58c3c2eaa9f860a3d708a0516
lavanya <lavanya.bhadri>
2022-01-29 07:55:27 +0530

    LISTENER

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -12,3 +12,20 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   target_id        = element(local.INSTANCE_IDS, count.index)
   port             = var.PORT
 }
+
+resource "aws_lb_listener_rule" "private" {
+  count        = var.IS_PRIVATE_LB ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = var.LB_RULE_PRIORITY
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
+  }
+}
