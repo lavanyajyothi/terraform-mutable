1a3410b140d2c2565a13697a4e8ce46b38db4b81
lavanya <lavanya.bhadri>
2022-01-29 06:28:16 +0530

    deploy

--- app-module/app-setup.tf
+++ app-module/app-setup.tf
@@ -1,9 +1,5 @@
 resource "null_resource" "app-deploy" {
   count = length(local.PRIVATE_IPS)
-  //triggers = {
-    //private_ip = element(local.PRIVATE_IPS, count.index)
-   /// abc = timestamp()
-  //}
   provisioner "remote-exec" {
     connection {
       host     = element(local.PRIVATE_IPS, count.index)
