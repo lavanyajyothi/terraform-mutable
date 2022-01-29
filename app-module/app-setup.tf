ea585bb56859c3ccdf7ff9b522fca523301523b4
lavanya <lavanya.bhadri>
2022-01-29 06:21:37 +0530

    deploy

--- app-module/app-setup.tf
+++ app-module/app-setup.tf
@@ -1,14 +1,18 @@
 resource "null_resource" "app-deploy" {
-  count     = length(local.PRIVATE_IPS)
+  count = length(local.PRIVATE_IPS)
+  //triggers = {
+    //private_ip = element(local.PRIVATE_IPS, count.index)
+   /// abc = timestamp()
+  //}
   provisioner "remote-exec" {
     connection {
-      host      = element(local.PRIVATE_IPS, count.index)
-      user      = local.ssh_user
-      password  = local.ssh_pass
+      host     = element(local.PRIVATE_IPS, count.index)
+      user     = local.ssh_user
+      password = local.ssh_pass
     }
 
     inline = [
-      "ansible-pull -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/ansible roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT}-e APP_VERSION= "
+      "ansible-pull -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/ansible roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT}"
     ]
   }
 }
