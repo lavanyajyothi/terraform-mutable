c5881dd68b744990d072f6b2f36a59c6f8c5bea9
lavanya <lavanya.bhadri>
2022-01-29 07:18:24 +0530

    deploy

--- app-module/vars.tf
+++ app-module/vars.tf
@@ -4,6 +4,8 @@ variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "PORT" {}
+variable "IS_PRIVATE_LB" {}
+variable "LB_RULE_PRIORITY" {}
 variable "APP_VERSION" {}
 variable "NEXUS_USER" {}
 variable "NEXUS_PASS" {}
