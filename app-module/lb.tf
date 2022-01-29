c4375e1b16bb8babebab20aee7c475c73a7e14ec
lavanya <lavanya.bhadri>
2022-01-29 08:10:11 +0530

    LISTENER

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -13,8 +13,6 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-}
-
 resource "aws_lb_target_group_attachment" "tg-attach" {
   count            = length(local.INSTANCE_IDS)
   target_group_arn = aws_lb_target_group.tg.arn
