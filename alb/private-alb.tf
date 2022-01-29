8c187e1db6590f6ffb230b88a0e962b6842f170b
lavanya <lavanya.bhadri>
2022-01-29 07:47:00 +0530

    deploy

--- alb/private-alb.tf
+++ alb/private-alb.tf
@@ -12,4 +12,20 @@ resource "aws_lb" "private" {
   }
 }
 
+resource "aws_lb_listener" "private_listener" {
+  load_balancer_arn = aws_lb.private.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
+  }
+}
+
 
