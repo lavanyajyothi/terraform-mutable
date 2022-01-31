5c9326efffb136866efa37dd9a70d2a38b9f0228
lavanya <lavanya.bhadri>
2022-01-31 18:00:02 +0530

    catalogue

--- app-module/vars.tf
+++ app-module/vars.tf
@@ -5,7 +5,6 @@ variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "PORT" {}
 variable "IS_PRIVATE_LB" {}
-variable "LB_RULE_PRIORITY" {}
 variable "APP_VERSION" {}
 variable "NEXUS_USER" {}
 variable "NEXUS_PASS" {}
