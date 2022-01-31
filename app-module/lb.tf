060cb835ebdd5bb2184d78bba6460eaff44ac38e
lavanya <lavanya.bhadri>
2022-01-31 18:12:12 +0530

    catalogue

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -20,7 +20,6 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   port             = var.PORT
 }
 
-
 resource "aws_lb_listener_rule" "private" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = 100
