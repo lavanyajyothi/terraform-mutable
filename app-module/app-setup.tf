623ad6bec45d425e482d943a1dd872a8ea768ee2
lavanya <lavanya.bhadri>
2022-02-10 07:08:08 +0530

    pipeline code

--- app-module/app-setup.tf
+++ app-module/app-setup.tf
@@ -12,7 +12,7 @@ resource "null_resource" "app-deploy" {
     }
 
     inline = [
-      "systemctl restart sshd"
+      "systemctl restart sshd",
       "ansible-pull -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/ansible roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USER=${var.NEXUS_USER} -e NEXUS_PASS=${var.NEXUS_PASS}"
     ]
   }
