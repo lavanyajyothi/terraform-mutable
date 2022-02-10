cc4ccd074662079c33c42cef03bae45719024b2d
lavanya <lavanya.bhadri>
2022-02-10 07:28:24 +0530

    pipeline code

--- app-module/app-setup.tf
+++ app-module/app-setup.tf
@@ -12,7 +12,7 @@ resource "null_resource" "app-deploy" {
     }
 
     inline = [
-      "yum install -y npm",
+      "sudo yum install -y npm",
       "ansible-pull -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/ansible roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USER=${var.NEXUS_USER} -e NEXUS_PASS=${var.NEXUS_PASS}"
     ]
   }
